4e612639e4239244674ea0424690a9208c56e1bf
jamaluddin <jamaluddinumar4@gmail.com>
2021-06-14 19:27:41 +0800

    tugasroom1

---
+++ app/src/main/java/com/example/tugasroom/ui/NoteViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.tugasroom.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.tugasroom.db.note.Note
+import com.example.tugasroom.db.note.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note){
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note){
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note){
+        noteRepository.update(note)
+    }
+
+}
